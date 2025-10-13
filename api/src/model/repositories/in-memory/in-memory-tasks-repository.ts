f81318cdbde37878d269ec40bb2dd45dc9c86cab
ptkrnnan <ptk.rnnan02@gmail.com>
2025-10-13 17:57:36 -0300

    fix: tabulação indesejada

--- api/src/model/repositories/in-memory/in-memory-tasks-repository.ts
+++ api/src/model/repositories/in-memory/in-memory-tasks-repository.ts
@@ -2,7 +2,7 @@ import { Task } from "../../entities/task.js";
 import type { TasksRepository } from "../tasks-repository.js";
 
 export class InMemoryTasksRepository implements TasksRepository {
-        public items: Task[] = []
+    public items: Task[] = []
 
     async create(data: any): Promise<Task> {
         const task = new Task(data.title, data.description, data.priority, data.status)
