ef166734e6480287cf446b36932f1ac0a3319754
ptkrnnan <ptk.rnnan02@gmail.com>
2025-11-28 01:12:56 -0300

    removendo config prisma

--- api/prisma/migrations/20251126202715_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL DEFAULT '',
-    "priority" TEXT NOT NULL,
-    "status" TEXT NOT NULL DEFAULT 'pending'
-);
--- api/src/model/repositories/prisma/prisma-tasks-repository.ts
+++ api/src/model/repositories/prisma/prisma-tasks-repository.ts
@@ -1,44 +1,47 @@
 import type { UUID } from "crypto";
 import type { Task } from "../../entities/task.js";
 import type { TasksRepository } from "../tasks-repository.js";
-import { PrismaClient } from "@prisma/client";
 import { TaskMapper } from "../mappers/task-mapper.js";
-
-const prisma = new PrismaClient();
 
 export class PrismaTasksRepository implements TasksRepository {
     async create(data: Task): Promise<Task> {
-        const task = TaskMapper.toPrisma(data)
-        const createdTask = await prisma.task.create({
-            data: task
-        })
-        return TaskMapper.toDomain(createdTask)
+        // const task = TaskMapper.toPrisma(data)
+        // const createdTask = await prisma.task.create({
+        //     data: task
+        // })
+        // return TaskMapper.toDomain(createdTask)
+        throw new Error("Method not implemented.");
     }
 
     async update(id: UUID, data: Task): Promise<Task> {
-        const task = TaskMapper.toPrisma(data)
-        const updatedTask = await prisma.task.update({ where: { id }, data: task })
-        return TaskMapper.toDomain(updatedTask)
+        // const task = TaskMapper.toPrisma(data)
+        // const updatedTask = await prisma.task.update({ where: { id }, data: task })
+        // return TaskMapper.toDomain(updatedTask)
+        throw new Error("Method not implemented.");
     }
 
     async delete(id: UUID): Promise<void> {
-        await prisma.task.delete({ where: { id } })
+        // await prisma.task.delete({ where: { id } })
+        throw new Error("Method not implemented.");
     }
 
     async findByTitle(title: string): Promise<Task | null> {
-        const task = await prisma.task.findFirst({ where: { title } })
-        if (!task) return null
-        return TaskMapper.toDomain(task)
+        // const task = await prisma.task.findFirst({ where: { title } })
+        // if (!task) return null
+        // return TaskMapper.toDomain(task)
+        throw new Error("Method not implemented.");
     }
 
     async findById(id: UUID): Promise<Task | null> {
-        const task = await prisma.task.findUnique({ where: { id } })
-        if (!task) return null
-        return TaskMapper.toDomain(task)
+        // const task = await prisma.task.findUnique({ where: { id } })
+        // if (!task) return null
+        // return TaskMapper.toDomain(task)
+        throw new Error("Method not implemented.");
     }
 
     async findAll(): Promise<Task[]> {
-        const tasks = await prisma.task.findMany()
-        return tasks.map(TaskMapper.toDomain)
+        // const tasks = await prisma.task.findMany()
+        // return tasks.map(TaskMapper.toDomain)
+        throw new Error("Method not implemented.");
     }
 }
